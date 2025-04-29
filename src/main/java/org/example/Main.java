33ae7beab40a49a398c5bc69ffb3202c7282e9ee
Michael Klymchuk <6312398@edu.vaniercollege.qc.ca>
2025-04-29 15:06:22 -0400

    implemented selectionSort method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,4 +20,28 @@ public class Main {
             }
         }
     }
+
+    /*
+    315214
+    135214
+    113524
+    112534
+    112354
+    112345
+     */
+
+    public static void selectionSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            int min = i;
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+
+            int store = nums[i];
+            nums[min] = store;
+            nums[min] = store;
+        }
+    }
 }
